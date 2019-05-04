ff81b3926ad14edb342a5f2c4721848721d17de7
DanielRoa <danbron523@gmail.com>
2019-05-04 15:19:07 -0500

    construi string

--- src/matrizcaracol/MatrizCaracol.java
+++ src/matrizcaracol/MatrizCaracol.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 public class MatrizCaracol {
     
-   public static void leerDocumento(BufferedReader doc, ArrayList<String> cadena) throws FileNotFoundException, IOException{
+   public static void leerDocumento(BufferedReader doc, ArrayList<String> cadena, ArrayList<String> cadenaTemporal, char [] caracteres) throws FileNotFoundException, IOException{
        
        
        
@@ -23,17 +23,32 @@ public class MatrizCaracol {
                     }
                     
                 }
-
                 
-                
-                System.out.println(cadena.get(3));
+                System.out.println(cadena);
+                System.out.println(cadena.toString().replace("[", "").replace("]", "").replace(" ", "").replace(",", ""));
+                System.out.println(reconstruccionString4(reconstruccionString3(reconstruccionString2(reconstruccionString(cadena.toString())))));
                 
     }
+   
+   public static String reconstruccionString(String s){
+       return s.replace("]","");
+   }
+   
+   public static String reconstruccionString2(String s){
+       return s.replace("[","");
+   }
+   
+   public static String reconstruccionString3(String s){
+       return s.replace(",","");
+   }
+   
+   public static String reconstruccionString4(String s){
+       return s.replace(" ","");
+   }
      
     public static void main(String[] args) throws FileNotFoundException, IOException{        
         
-                leerDocumento(new BufferedReader(new FileReader("C:\\Users\\danbr\\Documents\\NetBeansProjects\\MatrizCaracol\\src\\matrizcaracol\\archivo.txt")), new ArrayList<String>());
-                
+                leerDocumento(new BufferedReader(new FileReader("C:\\Users\\danbr\\Documents\\NetBeansProjects\\MatrizCaracol\\src\\matrizcaracol\\archivo.txt")), new ArrayList<String>(), new ArrayList<String>(), new char[25]);
                 
                 }
         
